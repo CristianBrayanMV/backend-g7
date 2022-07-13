5f744a661cbabcc3a0ec1714b32e8e7d4a3c488b
CristianBrayanMV <cristian.brayan.m.v@gmail.com>
2022-07-13 18:05:16 -0500

    reactive

--- application/src/App.js
+++ application/src/App.js
@@ -5,66 +5,124 @@ import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Button from '@mui/material/Button';
 import fondo from './img/form.webp';
+import { useState } from 'react';
 import './App.css';
 
 
 function App() {
+ 
+  const [input, setInput] = useState({
+    name:'',
+    lastName:'',
+    email:'',
+    phone:'',
+    password:'',
+    comment:'',
+    zone:'g',
+    aceptTerm:true
+  })
+  const [check, setCheck] = useState(false)
+  const handleChange = ({target}) =>{
+    setInput({
+      ...input,
+      [target.name]:target.value
+    });
+  }
+
+  const handleChangeChecks = ({target}) =>{
+    setCheck(!check)
+  }
+
+  const sendForm = (e) => {
+    e.preventDefault();
+    console.log({...input, aceptTerm: check})
+  }
+
   return (
     <div className='application'>
       <img src={fondo} alt="formulario" />
-      <form>
+      <form onSubmit={sendForm}>
         <h2>Registro</h2>
         <TextField
             id="outlined-text-input"
             label="Nombre"
             type="text"
+            name="name"
+            value={input.name}
+            onChange={handleChange}
             autoComplete="current-text"
           />
         <TextField
             id="outlined-lastname-input"
             label="Apellido"
             type="text"
+            name="lastName"
+            value={input.lastName}
+            onChange={handleChange}
             autoComplete="current-lastname"
           />
         <TextField
             id="outlined-email-input"
             label="Correo Electronico"
             type="email"
+            name="email"
+            value={input.email}
+            onChange={handleChange}
             autoComplete="current-email"
           />
         <TextField
             id="outlined-phone-input"
             label="Teléfono"
             type="phone"
+            name="phone"
+            value={input.phone}
+            onChange={handleChange}
             autoComplete="current-phone"
           />
         <TextField
             id="outlined-password-input"
             label="Contraseña"
             type="password"
+            name="password"
+            value={input.password}
+            onChange={handleChange}
             autoComplete="current-password"
           />
           <h3>Zonas</h3>
           <RadioGroup
             aria-labelledby="demo-radio-buttons-group-label"
-            defaultValue="female"
+            defaultValue="sp"
             name="radio-buttons-group"
           >
-            <FormControlLabel value="sp" control={<Radio />} label="Super Vip" />
-            <FormControlLabel value="v" control={<Radio />} label="Vip" />
-            <FormControlLabel value="g" control={<Radio />} label="General" />
+            <FormControlLabel value="sp"
+                              name="zone"
+                              onChange={handleChange}
+                             control={<Radio />} label="Super Vip" />
+            <FormControlLabel value="v" 
+                              name="zone"
+                              onChange={handleChange}
+                              control={<Radio />} label="Vip" />
+            <FormControlLabel value="g" 
+                              name="zone"
+                              onChange={handleChange}
+                              control={<Radio />} label="General" />
           </RadioGroup>
           <TextField
             id="outlined-comment-input"
             label="Comentario"
             multiline
             maxRows={4}
+            name="comment"
+            value={input.comment}
+            onChange={handleChange}
             autoComplete="current-comment"
           />          
-          <FormControlLabel control={<Checkbox defaultChecked />} label="Acepto los terminos y condiciones" />
+          <FormControlLabel control={<Checkbox  
+                                  checked={check}
+                                  onClick={handleChangeChecks} />} label="Acepto los terminos y condiciones" />
           <hr />
           <div className="btn">
-            <Button variant="contained" color="primary">
+            <Button variant="contained" color="primary" type="submit">
               Registrame
             </Button>
 
