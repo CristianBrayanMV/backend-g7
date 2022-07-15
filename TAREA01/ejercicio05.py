a4cf839341e4440b7207c3b26791e5d4851fb89d
CristianBrayanMV <cristian.brayan.m.v@gmail.com>
2022-07-15 12:02:01 -0500

    flask-concierto

--- TAREA01/ejercicio05.py
+++
@@ -1,7 +0,0 @@
-## ejercicio  05
-##texto que se debe ingresar -- hola alumnos buenas noches ya se viene el break
-print("PROGRAMA QUE SI DETERMINADO UN TEXTO TE RESPONDA")
-texto = input("Porfavor ingresa una texto especifico: ")
-texto = texto.title()
-
-print(texto.split())
